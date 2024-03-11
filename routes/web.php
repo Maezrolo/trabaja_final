2372103945b3ed629285f3fc72cd09802fd600be
Maezrolo <marcosuse007@gmail.com>
2024-03-11 18:20:21 -0300

    crear login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return 'Hola';
+});
+
+Route::view('/login',"login")->name('login');
